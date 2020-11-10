397aa4a30e5872d87980c0c4c6d6c4b0b0bca0af
dantxal <dantxal@gmail.com>
2020-11-10 14:22:28 -0300

    Refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import GooglePlacesMock from 'google-maps-places-mock'
 import { AiFillIdcard, AiFillClockCircle } from 'react-icons/ai'
 import { ChangeEvent, useCallback, useEffect, useState } from 'react'
@@ -8,12 +9,15 @@ export default function Home(): React.ReactElement {
   const [lastSearch, setLastSearch] = useState({ term: '', rating: 0 })
   const [search, setSearch] = useState('')
   const [filterRating, setFilterRating] = useState(0)
-  const [position, setPosition] = useState(null)
+  const [position, setPosition] = useState<Position>()
   const [places, setPlaces] = useState([])
 
   useEffect(() => {
     if (navigator?.geolocation) {
-      navigator.geolocation.getCurrentPosition(setPosition)
+      navigator.geolocation.getCurrentPosition((pos: Position) => {
+        setPosition(pos)
+        getPlaces(pos)
+      })
     }
   }, [])
 
@@ -21,25 +25,32 @@ export default function Home(): React.ReactElement {
     setSearch(e.target.value)
   }, [])
 
-  const handleFormSubmit = async e => {
-    e.preventDefault()
-
-    // ! Decided to use this mock since the places API has no free plan
+  async function getPlaces(pos: Position) {
+    if (!pos) return
     const request = new GooglePlacesMock(
       {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
       },
       500
     )
     const response = await request.nearbySearch()
+
     const nonDuplicate = response.filter(
       (place, index) =>
         response.map(item => item.id).indexOf(place.id) === index
     )
-    const filtered = nonDuplicate.filter(
-      place => place.rating >= filterRating / 5
-    )
+    const data = nonDuplicate.filter(place => place.rating >= filterRating / 5)
+
+    setPlaces(data)
+    setLastSearch({ term: search, rating: filterRating })
+  }
+
+  const handleFormSubmit = async e => {
+    e.preventDefault()
+
+    // ! Decided to use this mock since the places API has no free plan
+    getPlaces(position)
 
     // ? This is what the response would look like if it were to use the api
     // const response = await api.get('/nearbysearch/json', {
@@ -48,22 +59,20 @@ export default function Home(): React.ReactElement {
     //     radius: 500
     //   }
     // })
-    console.log('rating', filterRating)
-    console.log('search', search)
-    setPlaces(filtered)
-    setLastSearch({ term: search, rating: filterRating })
   }
 
   return (
-    <div className="w-screen flex flex-col justify-center h-full">
+    <div className="w-full flex flex-col justify-center h-full">
       <Head>
-        <title>Create Next App</title>
+        <title>findplaces | Demo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="flex max-w-screen-xl px-10 py-5 justify-between ">
-        <div className="">findplaces</div>
-        <div>home</div>
+        <div className="font-semibold">findplaces</div>
+        <Link href="/">
+          <a>home</a>
+        </Link>
       </div>
 
       <form
@@ -102,27 +111,32 @@ export default function Home(): React.ReactElement {
             lastSearch.rating
           } stars`}
         </h1>
+
         {places.map(place => (
           <div
             key={place.id}
             className="bg-white w-full max-w-6xl rounded p-5 flex space-x-5"
           >
             <img src="/schoolPic.png" alt="" className="rounded" />
+
             <div className="flex-grow space-y-1">
               <div className="flex justify-between w-full">
                 <h2>{place.name}</h2>
                 <Rating value={place.rating * 5} readOnly />
               </div>
+
               <div className="flex space-x-3 m">
                 <div className="flex space-x-1 items-center text-gray-600 text-sm">
                   <AiFillIdcard size={16} color="#22a8e1" />
                   <p>R. Álvares de Azevedo, 50</p>
                 </div>
+
                 <div className="flex space-x-1 items-center text-gray-600 text-sm">
                   <AiFillClockCircle size={16} color="#00b8d1" />
                   <p>10 minutes</p>
                 </div>
               </div>
+
               <div className="text-gray-700">
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
